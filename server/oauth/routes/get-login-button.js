c0f1dd147c50ef16e0b1af3d107ea59b41f194e1
d34thwings <d34thwings@gmail.com>
2017-06-14 12:26:26 +0000

    feat(oauth): Add sso button route

---
+++ server/oauth/routes/get-login-button.js
@@ -0,0 +1,17 @@
+module.exports = {
+  method: 'GET',
+  path: '/oauth/sso-button',
+  config: {
+    auth: {
+      strategies: ['session'],
+      mode: 'optional',
+    },
+    plugins: {
+      'hapi-auth-cookie': { redirectTo: false },
+    },
+  },
+  handler(req, res) {
+    const context = req.auth.isAuthenticated ? req.auth.credentials.toJSON() : {};
+    return res.view('oauth-login-button', context, { layout: false });
+  },
+};
--- server/oauth/routes/index.js
+++ server/oauth/routes/index.js
@@ -2,10 +2,12 @@ const getAssets = require('./get-assets');
 const getAuthorize = require('./get-authorize');
 const postAuthorize = require('./post-authorize');
 const postToken = require('./post-token');
+const getLoginButton = require('./get-login-button');
 
 module.exports = [
   getAssets,
   getAuthorize,
   postAuthorize,
   postToken,
+  getLoginButton,
 ];
